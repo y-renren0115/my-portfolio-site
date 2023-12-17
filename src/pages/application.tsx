cae6587c9d1b3ba9dc4cd9011451b525dea328f5
Ren Yoshizawa <renyoshizawa0515@gmail.com>
2023-12-17 23:48:31 +0900

    UIデザインの変更を実施しました。

--- src/pages/application.tsx
+++ src/pages/application.tsx
@@ -1,32 +1,75 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import Card from '../components/Card';
 
-const ContactPage: React.FC = () => {
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const cardsData = [
+    {
+      title: 'Card Title 1',
+      description: 'This is the card description.'
+    },
+    {
+      title: 'Card Title 2',
+      description: 'This is another card description.'
+    },
+    {
+      title: 'Card Title 3',
+      description: 'Yet another card description.'
+    }
+  ];
+  
+
   return (
-    <Layout>
-      <div className="max-w-2xl mx-auto p-8">
-        <h1 className="text-4xl font-bold text-center mb-4">Contact Me</h1>
-        <p className="text-center mb-8">If you have any questions, feel free to reach out to me.</p>
+    <div className="flex flex-col min-h-screen">
+      <header className="flex items-center bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* ロゴ部分 */}
+          <div className="flex items-center">
+            <Image src="/logo.png" alt="Logo" width={50} height={50} />
+            <span className="font-bold text-xl ml-2">My Portfolio</span>
+          </div>
 
-        <form className="space-y-6">
-          <div>
-            <label htmlFor="name" className="text-sm font-medium text-gray-700">Name</label>
-            <input type="text" id="name" className="mt-1 p-2 w-full border rounded" />
-          </div>
-          <div>
-            <label htmlFor="email" className="text-sm font-medium text-gray-700">Email</label>
-            <input type="email" id="email" className="mt-1 p-2 w-full border rounded" />
-          </div>
-          <div>
-            <label htmlFor="message" className="text-sm font-medium text-gray-700">Message</label>
-            <textarea id="message" className="mt-1 p-2 w-full border rounded" rows={4}></textarea>
-          </div>
-          <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Send Message</button>
-        </form>
+          {/* ナビゲーションリンク */}
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/about" passHref>
+                  <span className="hover:underline cursor-pointer">About</span>
+                </Link>
+              </li>
+              <li>
+                <Link href="/works" passHref>
+                  <span className="hover:underline cursor-pointer">Works</span>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact" passHref>
+                  <span className="hover:underline cursor-pointer">Contact</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main className="flex-grow container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {cardsData.map((card, index) => (
+          <Card key={index} title={card.title} description={card.description} />
+        ))}
       </div>
-    </Layout>
+    </main>
+
+      <footer className="bg-gray-800 text-white text-center p-4">
+        <p>&copy; {new Date().getFullYear()} My Portfolio</p>
+      </footer>
+    </div>
   );
 };
 
-export default ContactPage;
+export default Layout;
